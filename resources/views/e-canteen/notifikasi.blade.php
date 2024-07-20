d0b8ae8452c61e6d3513668da855d6eb758ccd60
SAIFUL RIKI YULIANTO <fauziricky370@gmail.com>
2024-07-20 16:57:54 +0700

    notif

--- resources/views/e-canteen/notifikasi.blade.php
+++ resources/views/e-canteen/notifikasi.blade.php
@@ -3,9 +3,12 @@
 
     @section('contents')
     <main class="relative bg-gray-100  ">
-        <article class="w-[92%] mx-auto bg-white md:py-10 xs:py-4 xs:px-3 xl:px-16 gap-12">
+        <article class="w-[92%] mx-auto bg-white md:py-10 xs:py-4 xs:px-3 xl:px-16 gap-12 relative">
+            <h1 class="font-bold text-xl text-slate-600 mb-4">Notifikasi</h1>
             <section>
-                <button class="flex justify-end border-b border-slate-300 py-4">Tandai sudah dibaca semua</button>
+                <button class="absolute top-16 right-16 text-black hover:text-blue-600 ">
+                    Tandai sudah dibaca semua
+                </button> 
                 <div class="flex items-center border-b border-slate-300 py-4">
                     <div class="mr-4">
                         <img src="./assets/image/warung1.svg" alt="" class="rounded-xl xs:w-16 md:w-20">
@@ -17,6 +20,7 @@
                     </div>
                 </div>
             </section>
+            
         </article>
     </main>
     @endsection
